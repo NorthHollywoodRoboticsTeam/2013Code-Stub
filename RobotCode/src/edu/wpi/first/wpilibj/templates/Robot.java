0e84ce86309c00056fbc24bbb42f45dce9ddfb84
Henry's Laptop <henryslaptop@ubuntu.ubuntu-domain>
2013-01-23 18:28:24 -0800

    Started to add print functionallity.

--- RobotCode/src/edu/wpi/first/wpilibj/templates/Robot.java
+++ RobotCode/src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -1,5 +1,7 @@
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.DriverStationLCD;
+
 public class Robot {
     private static Robot instance = new Robot();
     public static Robot getRobot() {
@@ -15,6 +17,31 @@ public class Robot {
     
     public DriveTrain getDriveTrain() {
 	return driveTrain;
+    }
+    
+    
+    
+    /*public void printToLCD(String text) {
+        
+    
+    }*/
+    
+    public void printBlockToLCD(String text) {
+        
+    
+    	
+    
+    }
+    private DriverStationLCD lcd = DriverStationLCD.getInstance();
+    private String textOnScreen = "";
+    int lineCount = 0;
+    public void printlnToLCD(String text) {
+        textOnScreen = textOnScreen + text;
+        lineCount++;
+        if ()
+    
+    	
+        printToLCD("\n" + text);
     }
     
 }
